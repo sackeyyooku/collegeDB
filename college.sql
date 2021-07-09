7d509c513532036149ae5a9c685ac2999186bf18
Konlan Gisele <gisele.konlan@ashesi.edu.gh>
2021-07-09 14:28:42 +0000

    added tables for  Lecturer, Lecturer_Course and Department

--- college.sql
+++ college.sql
@@ -10,4 +10,19 @@ create table students(
     name varchar(100) not null
 )
 
--- create results table
+create table Lecturer(
+    id int autoincrement primary key,
+Lecturer_name varchar(120),
+Lecturer_id varchar(15),
+Lecturer_DOB varchar(50) unique,
+primary key(Lecturer_id));
+
+create table Lecturer_Courses(
+    id int autoincrement primary key,
+Course_id varchar(50),
+Lecturer_id varchar(15));
+
+create table Department(
+    id int autoincrement primary key,
+Department_Name  varchar(23),
+primary key(Department_Name));
