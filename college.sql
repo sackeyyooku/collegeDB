525012e54bcc76cee6f6237608e79d5f25240a83
Yooku Sackey <sackeyyooku@gmail.com>
2021-07-09 17:54:14 +0000

    change7

--- college.sql
+++ college.sql
@@ -4,38 +4,43 @@ create database college;
 use college;
 
 create table Student(
+id auto_increment,
 firstname varchar(150) NOT NULL,
 lastname varchar(150) NOT NULL,
 student_id int(10) NOT NULL,
 dob date NOT NULL,
-PRIMARY KEY (student_id)
+PRIMARY KEY(id)
 );
 
 create table Course(
 course_id int(50),
 course_name varchar(150),
-PRIMARY KEY (course_id)
+PRIMARY KEY(id)
 );
 
 create table StudentCourse(
+id auto_increment,
 course_id int(50),
 student_id int(50),
 FOREIGN KEY (course_id)
 references Course(student_id),
 FOREIGN KEY (student_id)
-references Student(student_id)
+references Student(student_id),
+PRIMARY KEY(id)
 );
 
 create table Lecturer(
+id auto_increment,
 lecturer_id varchar(15),
 firstname varchar(120),
 lastname varchar(120),
 dob varchar(50) unique,
 contact varchar(255),
-PRIMARY KEY (lecturer_id)
+PRIMARY KEY (id)
 );
 
 create table LecturerCourses(
+id auto_increment,
 course_id varchar(50),
 lecturer_id varchar(15),
 FOREIGN KEY (course_id)
@@ -45,11 +50,13 @@ references Lecturer(lecturer_id)
 );
 
 create table Department(
+id auto_increment,
 department_name  varchar(255),
-PRIMARY KEY (department_name)
+PRIMARY KEY (id)
 );
 
 create table Result(
+id auto_increment,
 score int primary key,
 lecturer_id int(20) not null,
 student_id int(20) not null,
