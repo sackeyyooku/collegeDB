9703ad8f3e1e4e15dd2ea10d7d8f7460a7e65c71
BaniEmefa <82719086+BaniEmefa@users.noreply.github.com>
2021-07-09 15:39:18 +0000

    changes2

--- college.sql
+++ college.sql
@@ -6,30 +6,47 @@ create database college;
 
 -- create student table
 create table students(
-    id int primary key,
-    name varchar(100) not null
+first_name varchar(150),
+last_name varchar(150),
+student_id int(50),
+student_DOB varchar(50) unique,
+primary key (student_id));
 );
 
-create table results(
-    score int primary key,
-    lecturer_id int not null
+create table student_courses(
+course_id int(50),
+student_id int(50)
 );
 
+create table courses(
+course_id int(50),
+department_name varchar(150),
+course_name varchar(150)
+);
 
 create table Lecturer(
-    id int primary key,
+id int primary key,
 Lecturer_name varchar(120),
 Lecturer_id varchar(15),
 Lecturer_DOB varchar(50) unique,
 primary key(Lecturer_id));
 
 create table Lecturer_Courses(
-    id int autoincrement primary key,
+id int autoincrement primary key,
 Course_id varchar(50),
-L888ecturer_id varchar(15));
+Lecturer_id varchar(15)
+);
 
 create table Department(
-    id int autoincrement primary key,
+id int autoincrement primary key,
 Department_Name  varchar(23),
 primary key(Department_Name));
 );
+
+create table results(
+score int primary key,
+lecturer_id int not null,
+student_id int not null,
+course_id
+);
+
