c8f50ae99ce723bee912028dfdc2c0a61cd0c4e7
Yooku Sackey <sackeyyooku@gmail.com>
2021-07-09 18:43:47 +0000

    change7

--- college.sql
+++ college.sql
@@ -4,7 +4,7 @@ create database college;
 use college;
 
 create table Student(
-id auto_increment,
+id int auto_increment,
 firstname varchar(150) NOT NULL,
 lastname varchar(150) NOT NULL,
 student_id int(10) NOT NULL,
@@ -13,24 +13,25 @@ PRIMARY KEY(id)
 );
 
 create table Course(
-course_id int(50),
+id int auto_increment,
+course_id varchar(255),
 course_name varchar(150),
 PRIMARY KEY(id)
 );
 
 create table StudentCourse(
-id auto_increment,
+id int auto_increment,
 course_id int(50),
 student_id int(50),
 FOREIGN KEY (course_id)
-references Course(student_id),
+references Course(id),
 FOREIGN KEY (student_id)
-references Student(student_id),
+references Student(id),
 PRIMARY KEY(id)
 );
 
 create table Lecturer(
-id auto_increment,
+id int auto_increment,
 lecturer_id varchar(15),
 firstname varchar(120),
 lastname varchar(120),
@@ -40,31 +41,33 @@ PRIMARY KEY (id)
 );
 
 create table LecturerCourses(
-id auto_increment,
-course_id varchar(50),
-lecturer_id varchar(15),
+id int auto_increment,
+course_id int,
+lecturer_id int,
 FOREIGN KEY (course_id)
-references Course(course_id),
+references Course(id),
 FOREIGN KEY (lecturer_id)
-references Lecturer(lecturer_id)
+references Lecturer(id),
+PRIMARY KEY (id)
 );
 
 create table Department(
-id auto_increment,
+id int auto_increment,
 department_name  varchar(255),
 PRIMARY KEY (id)
 );
 
 create table Result(
-id auto_increment,
-score int primary key,
-lecturer_id int(20) not null,
+id int auto_increment,
+score int,
+lecturer_id int(20),
 student_id int(20) not null,
 course_id int (50) not null,
 FOREIGN KEY (course_id)
-references Course(course_id),
+references Course(id),
 FOREIGN KEY (lecturer_id)
-references Lecturer(lecturer_id),
+references Lecturer(id),
 FOREIGN KEY (course_id)
-references Course(course_id)
+references Course(id),
+PRIMARY KEY (id)
 );
