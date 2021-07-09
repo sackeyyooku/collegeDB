a55ea9d264ffe0b5bfafdee2980071b284f8f2eb
Konlan Gisele <gisele.konlan@ashesi.edu.gh>
2021-07-09 15:20:45 +0000

    changes

--- college.sql
+++ college.sql
@@ -1,4 +1,4 @@
---drop db first
+
 drop database if exists college; 
 
 -- create database
@@ -6,11 +6,30 @@ create database college;
 
 -- create student table
 create table students(
-    id int autoincrement primary key,
+    id int primary key,
     name varchar(100) not null
-)
+);
 
 create table results(
     score int primary key,
     lecturer_id int not null
-)
+);
+
+
+create table Lecturer(
+    id int primary key,
+Lecturer_name varchar(120),
+Lecturer_id varchar(15),
+Lecturer_DOB varchar(50) unique,
+primary key(Lecturer_id));
+
+create table Lecturer_Courses(
+    id int autoincrement primary key,
+Course_id varchar(50),
+L888ecturer_id varchar(15));
+
+create table Department(
+    id int autoincrement primary key,
+Department_Name  varchar(23),
+primary key(Department_Name));
+);
