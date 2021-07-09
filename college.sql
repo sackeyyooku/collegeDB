be2e993257a43a312096fa16bc533a787483005c
Konlan Gisele <gisele.konlan@ashesi.edu.gh>
2021-07-09 14:57:11 +0000

    create tbles for lecturer etc

--- college.sql
+++ college.sql
@@ -10,7 +10,6 @@ create table students(
     name varchar(100) not null
 )
 
-<<<<<<< HEAD
 create table Lecturer(
     id int autoincrement primary key,
 Lecturer_name varchar(120),
@@ -27,9 +26,3 @@ create table Department(
     id int autoincrement primary key,
 Department_Name  varchar(23),
 primary key(Department_Name));
-=======
-create table results(
-    score int primary key,
-    lecturer_id int not null
-)
->>>>>>> 259797bd574589a926d0a4332c4dcc4ea388d449
