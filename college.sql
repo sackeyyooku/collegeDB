9d9f4c5f5c6b7eb6bb33a1099b42518888a46ba2
Yooku Sackey <sackeyyooku@gmail.com>
2021-07-12 16:37:42 +0000

    final change 2

--- college.sql
+++ college.sql
@@ -65,41 +65,40 @@ FOREIGN KEY (course_id) references Course(id),
 PRIMARY KEY (id)
 );
 
-insert into Student value("1","Kwame","Asamoah","2254","1999-08-11");
-insert into Student value("2","Kwesi","Anah","1114","2000-05-23");
-insert into Student value("3","Kofi","	Boye","3274","2002-03-01");
+insert into Student value(1,"Kwame","Asamoah",2254,"1999-08-11");
+insert into Student value(2,"Kwesi","Anah",1114,"2000-05-23");
+insert into Student value(3,"Kofi","Boye",3274,"2002-03-01");
 
-insert into Course value ("1","444","BIOLOGY 444");
-insert into Course value ("2","789","MATH 789");
-insert into Course value ("3","231","FRENCH 231");
+insert into Course value (1,444,"BIOLOGY 444");
+insert into Course value (2,789,"MATH 789");
+insert into Course value (3,231,"FRENCH 231");
 
+insert into Lecturer value (1,"KO123","Kofi","Nti","1975-05-01",020120155);
+insert into Lecturer value (2,"NI456","Nii","Amo","1965-06-03",02561962);
+insert into Lecturer value (3,"AL789","Ali","Owusu","1980-08-09",026332145);
 
+insert into StudentCourse value (1,1,1);
+insert into StudentCourse value (2,2,2);
+insert into StudentCourse value (3,3,3);
 
-insert into Lecturer value ("1","KO123","Kofi","Nti","1975-05-01","020120155");
-insert into Lecturer value ("2","NI456","Nii","Amo","1965-06-03","02561962");
-insert into Lecturer value ("3","AL789","Ali","Owusu","1980-08-09","026332145");
+insert into LecturerCourse value (1,1,1); 
+insert into LecturerCourse value (2,2,2);
+insert into LecturerCourse value (3,3,3);
 
-insert into StudentCourse value ("1","1","1");
-insert into StudentCourse value ("2","2","2");
-insert into StudentCourse value ("3","3","3");
+insert into Department value (1,"MATH");
+insert into Department value (2,"PHYSICS");
+insert into Department value (3,"FRENCH");
 
-insert into LecturerCourse value ("1","1","1"); 
-insert into LecturerCourse value ("2","2","2");
-insert into LecturerCourse value ("3","3","3");
-
-insert into Department value ("1","MATH");
-insert into Department value ("2","PHYSICS");
-insert into Department value ("3","FRENCH");
-
-insert into Result value ("1","67","1","8939","1");
-insert into Result value ("2","89","2","5665","2");
-insert into Result value ("3","45","3","4546","3");
+insert into Result value (1,67,1,8939,1);
+insert into Result value (2,89,2,5665,2);
+insert into Result value (3,45,3,4546,3);
 
 SELECT student_id, score,
 CASE
-WHEN score < 20 then 'F' 
-    WHEN score < 40 then 'D'
-    WHEN score < 60 then 'C'
+WHEN score < 40 then 'F' 
+    WHEN score < 50 then 'E'
+    WHEN score < 60 then 'D'
+    WHEN score < 70 then 'C'
     WHEN score < 80 then 'B'
     ELSE 'A'
 END AS grade
